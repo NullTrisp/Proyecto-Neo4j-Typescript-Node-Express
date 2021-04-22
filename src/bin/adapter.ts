ea07ace59b49d80d021007abacfd8c29973c5eaf
Nicolás Calvache Jiménez <nicolas.calvache@alumnos.uneatlantico.es>
2021-04-22 17:15:19 +0200

    changed variable names

--- src/bin/adapter.ts
+++ src/bin/adapter.ts
@@ -1,7 +1,7 @@
 import neo4j from "neo4j-driver";
 import { CONSTANTS } from "./constants";
 
-export const driver = neo4j.driver(
+export const DBDRIVER = neo4j.driver(
   CONSTANTS.DBURI,
   neo4j.auth.basic(CONSTANTS.DBUSER, CONSTANTS.DBPASS)
 );
--- src/bin/constants.ts
+++ src/bin/constants.ts
@@ -4,5 +4,5 @@ config();
 export const CONSTANTS = {
   DBURI: process.env.DBURI as string,
   DBUSER: process.env.DBUSER as string,
-  DBPASS: process.env.DBURI as string,
+  DBPASS: process.env.DBPASS as string,
 };
